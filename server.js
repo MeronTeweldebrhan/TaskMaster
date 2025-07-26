12135d4fbffdeaa2845aaddb797c33d11586b2bb
MeronTeweldebrhan <meronqelati@gmail.com>
2025-07-26 14:18:44 -0400

    add readme file and update server.js

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use('/api/users',userRoutes)
 app.use('/api/projects',projectRoutes)
 app.use('/api/tasks', taskRoutes); 
 
+app.use('/',(req,res)=>{
+    res.json('Task master app runinig')
+})
+
 app.listen(PORT,()=>{
     console.log(`Server is runinig on port :${PORT}`)
 })
